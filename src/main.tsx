2657ef6ab92a178c3c646b3600b02cc69ae04774
Aspitrine <loquente62@gmail.com>
2023-11-09 10:56:21 +0100

    Ajout de react redux + configuration du Provider + hook pre typer

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,17 @@
+import { Provider } from 'react-redux';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './components/App/App';
 
 import './styles/index.scss';
+import store from './store';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    {/* J'englobe mon application du provider de react-redux afin de fournir les données du store à mon application */}
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
--- src/store/index.ts
+++ src/store/index.ts
@@ -9,3 +9,8 @@ const store = configureStore({
 });
 
 export default store;
+
+// Je déduis le type `RootState` et `AppDispatch` depuis le store lui même
+export type RootState = ReturnType<typeof store.getState>;
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch;
