623f53bf133bffa5831c4f6bd0198055f5e6a10d
Aspitrine <loquente62@gmail.com>
2023-11-13 09:55:52 +0100

    Ajout de la donnée initial sur l'etat d'ouverture de mon settings + récupération côté composant

--- src/components/Settings/Settings.tsx
+++ src/components/Settings/Settings.tsx
@@ -1,18 +1,23 @@
+import { useAppSelector } from '../../hooks/redux';
 import './Settings.scss';
 
 function Settings() {
+  // Pour récupérer les données provenant de mon store, j'utilise useAppSelector
+  const isSettingsOpened = useAppSelector((state) => state.settings.isOpen);
   return (
     <div className="settings">
       <button type="button" className="settings__toggle">
         X
       </button>
-      <form className="settings__form">
-        <input type="email" className="settings__input" />
-        <input type="password" className="settings__input" />
-        <button type="submit" className="settings__submit">
-          Envoyer
-        </button>
-      </form>
+      {isSettingsOpened && (
+        <form className="settings__form">
+          <input type="email" className="settings__input" />
+          <input type="password" className="settings__input" />
+          <button type="submit" className="settings__submit">
+            Envoyer
+          </button>
+        </form>
+      )}
     </div>
   );
 }
--- src/store/reducers/settings.ts
+++ src/store/reducers/settings.ts
@@ -1,8 +1,16 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+type SettingsState = {
+  isOpen: boolean;
+};
+
+const initialState: SettingsState = {
+  isOpen: true,
+};
+
 const settingsSlice = createSlice({
   name: 'settings',
-  initialState: {},
+  initialState,
   reducers: {},
 });
 
